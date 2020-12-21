411c614460f7818dedc8ee0b2f569ca0961bf408
rginsberg <rginsberg549@gmail.com>
2020-12-20 21:09:55 -0300

    updating app

--- react_portfolio_v1/src/components/Project.js
+++ react_portfolio_v1/src/components/Project.js
@@ -2,19 +2,24 @@ import React from 'react';
 
 function Project(props) {
     return(
-        <div className="row">
-            <div class="card">
-                <img class="card-img-top quicktrends-icon" src={props.imgURL} alt="Card image cap"
-          />
-          <div class="card-body mx-auto">
-            <h5 class="card-title">{props.projectTitle}</h5>
-            <p class="card-text">{[props.projectDescription]}
-            </p>
-            <a href={props.deployedAppURL} class="btn btn-primary my-3">View Deployed App</a>
-            <a href={props.githubURL} class="btn btn-secondary">View Github Repo</a>
-          </div>
+        <div className="container">
+        <div className="m-5">
+            <div className="">
+                    <img class="card-img-top quicktrends-icon" src={props.imgURL}></img>
+            </div>
+            
+            <div class="">
+                    <h5 className="card-title">{props.projectTitle}</h5>
+                    <p className="mx-auto card-text">{[props.projectDescription]}</p>
+            </div>
+
+            <div className="">
+                <a target="_blank" className="col-3 btn btn-primary m-3" href={props.deployedAppURL} >View Deployed App</a>
+                <a target="_blank" class="col-3 btn btn-secondary m-3" href={props.githubURL}>View Github Repo</a>
+                </div>
         </div>
-    </div>
+
+        </div>
     )
 
 }
--- react_portfolio_v1/src/components/Projects.js
+++ react_portfolio_v1/src/components/Projects.js
@@ -14,11 +14,19 @@ function Projects() {
         ></Project>
 
         <Project 
-        imgURL="./img/quicktrends-icon.png"
-        projectTitle="Project #1 - QuickTrends"
-        projectDescription="A simple vanilla JS project featuring trend detection, company financials and a NYT Newsfeed."
-        deployedAppURL="https://rginsberg549.github.io/project-1-v2/"
-        githubURL="https://github.com/rginsberg549/project-1-v2"
+        imgURL="./img/quicktrends-v2-screenshot.png"
+        projectTitle="Project #2 - QuickTrends - V2"
+        projectDescription="Project #1 extension built with Node JS hosted on Heroku. Adds user registration and authenication, search history, revamp UI."
+        deployedAppURL="https://quicktrends.herokuapp.com/"
+        githubURL="https://github.com/rginsberg549/quicktrends-v2"
+        ></Project>
+
+<Project 
+        imgURL="./img/rfa-screenshot.png"
+        projectTitle="Project #3 - Request For Authorization"
+        projectDescription="A React App with Node.js and MYSQL database to help doctors create, download, and submit the request for authorization"
+        deployedAppURL="https://rfav1.herokuapp.com/"
+        githubURL="https://github.com/rginsberg549/rfa_v1"
         ></Project>
     </div>
     )
